67cd4b732ca0a2a5731a4b6c046c06f39b338c51
Usfan Ali Khan <140039105+usfankhan@users.noreply.github.com>
2025-07-23 20:16:29 +0530

    Create SwapNumbers.java

---
+++ SwapNumbers.java
@@ -0,0 +1,12 @@
+public class swap{
+    public static void main(String args[]){
+        int a=10,b=5;
+        System.out.println("a="+a+" "+"b="+b);
+        System.out.println("SWAP");
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a+" "+"b="+b);
+    }
+}
